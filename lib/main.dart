66696478848163414fd956929c128823a27a2096
KaranDuggal <duggalkaran400@gmail.com>
2023-05-03 08:11:37 +0530

    linux build down

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:game1/home.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux) {
+    WindowManager.instance.setMinimumSize(const Size(400, 700));
+    WindowManager.instance.setMaximumSize(const Size(400, 700));
+    WindowManager.instance.isResizable();
+  }
   runApp(const MyApp());
 }
 
